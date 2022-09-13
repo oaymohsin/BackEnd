eb21c3d59b3862d5ace152bb46d515e41cfc8a64
oaymohsin <oaymohsin@gmail.com>
2022-09-13 13:33:38 -0700

    push

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
 const LoadMyEnvironmentVariable=require('./configuration/LoadMyEnvironmentVariable');
-port=process.env.NODE_ENV;
+port=process.env.port;
 app.listen(port,()=>{
     console.log(`your app is listed on port : ${port}`);
     console.log(process.env.NODE_ENV);
